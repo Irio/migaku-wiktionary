2ef1d826620c25a9c0897fa6aadbbf732fbb092e
Irio Musskopf <irio@protonmail.com>
2020-12-04 13:36:39 +0100

    Export a dictionary for each language available in the dump

--- commands.py
+++ commands.py
@@ -1,5 +1,7 @@
 import os
 
+import unidecode
+
 from migaku_wiktionary.exporter import Exporter
 from migaku_wiktionary.serializer import Serializer
 from migaku_wiktionary.xml_parser import XMLParser
@@ -12,9 +14,31 @@ def download():
 
 def convert(filepath, destinationpath):
     # Convert a pre-downloaded dump into a Migaku Dictionary.
-    pages = XMLParser(filepath).parse()
-    json = Serializer(pages).to_json()
-    destinationpath = os.path.join(
-        destinationpath, os.path.basename(filepath).replace(".xml", ".json")
-    )
-    Exporter(json).save(destinationpath)
+    def filter_dictionaries(pages_dict):
+        pages_dict = {
+            language: pages
+            for language, pages in pages_dict.items()
+            if language is not None and len(pages) > 20_000
+        }
+        return sorted(
+            pages_dict.items(),
+            key=lambda item: -len(item[1]),
+        )
+
+    def generate_output_path(xml_language, xml_path, output_language, output_folder):
+        xml_language = normalize_string(xml_language)
+        output_language = normalize_string(output_language)
+        basename = f"{xml_language}_{output_language}.json"
+        return os.path.join(output_folder, basename)
+
+    def normalize_string(string):
+        return unidecode.unidecode(string).lower()
+
+    xml = XMLParser(filepath)
+    pages_dict = xml.parse()
+    for language, pages in filter_dictionaries(pages_dict):
+        json = Serializer(pages).to_json()
+        json_path = generate_output_path(
+            xml.language, filepath, language, destinationpath
+        )
+        Exporter(json).save(json_path)
